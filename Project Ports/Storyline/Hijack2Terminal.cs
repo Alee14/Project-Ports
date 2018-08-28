0be5946ce559ea76df7301bcbea87f17f6487652
Andrew <alee14498@gmail.com>
2018-08-28 15:30:49 -0400

    added the hijack2terminal

---
+++ Project Ports/Storyline/Hijack2Terminal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Ports.Storyline
+{
+    public partial class Hijack2Terminal : Form
+    {
+        public Hijack2Terminal()
+        {
+            InitializeComponent();
+        }
+    }
+}
