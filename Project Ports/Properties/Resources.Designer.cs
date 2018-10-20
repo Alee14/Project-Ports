3daa45148fcf05185da473ea3f7ab4ec7f0b4366
Alee14 <alee14498@gmail.com>
2018-10-20 17:06:03 -0400

    stuff

--- Project Ports/Properties/Resources.Designer.cs
+++ Project Ports/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Project_Silicon.Properties {
+namespace Project_Ports.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Project_Silicon.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Project_Silicon.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Project_Ports.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- Project Ports/Properties/Settings.Designer.cs
+++ Project Ports/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Project_Silicon.Properties {
+namespace Project_Ports.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
